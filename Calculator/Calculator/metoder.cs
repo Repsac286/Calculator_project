8ccde2fb877f8b3a1d4c011774f4ab8098987ab9
TobiasAagaard <tach@trifork.com>
2026-04-20 12:31:30 +0200

    fjerner de 4 metoder så vi kan lave dem hver for sig

--- Calculator/Calculator/metoder.cs
+++ Calculator/Calculator/metoder.cs
@@ -12,24 +12,6 @@ namespace Calculator
         private int b;
 
 
-        public int add()
-        {
-            return a + b;
-        }
-
-        public int subtract()
-        {
-            return a - b;
-        }
-
-        public int multiply() 
-        { 
-            return a * b;
-        }
-
-        public int divide()
-        {
-            return a / b;
-        }
+        
     }
 }
