0221bc16368f03bbf755396ced0df337e4facb6b
Repsac286 <Casp8952@gmail.com>
2026-04-20 12:34:02 +0200

    Added addition and subtraction

--- Calculator/Calculator/metoder.cs
+++ Calculator/Calculator/metoder.cs
@@ -12,6 +12,14 @@ namespace Calculator
         private int b;
 
 
-        
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int subtract(int a,int b) 
+        {
+            return a - b; 
+        }
     }
 }
