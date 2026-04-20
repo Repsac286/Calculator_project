26a67c56e18589db20efc5804329e5acf5b7e3ba
Repsac286 <Casp8952@gmail.com>
2026-04-20 12:28:09 +0200

    added functions

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+static int add(int a, int b) { return a + b; }
---
+++ Calculator/Calculator/metoder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class metoder
+    {
+        private int a;
+        private int b;
+
+
+        public int add()
+        {
+            return a + b;
+        }
+
+        public int subtract()
+        {
+            return a - b;
+        }
+
+        public int multiply() 
+        { 
+            return a * b;
+        }
+
+        public int divide()
+        {
+            return a / b;
+        }
+    }
+}
