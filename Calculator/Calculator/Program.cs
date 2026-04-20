ad244d374e4e392d173ecaf6ca3fe67ffb48f9c7
TobiasAagaard <tach@trifork.com>
2026-04-20 13:07:17 +0200

    Calculator logic

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,5 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Calculator;
+double result = 0;
+metoder m = new metoder();
+Console.WriteLine("Skriv det første tal:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Skriv det andet tal:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Vælg en operation: +, -, *, /");
+string? operation = Console.ReadLine();
 
+switch (operation)
+    {
+        case "+":
+            result = m.add(a, b);
+            break;
+        case "-":
+            result = m.subtract(a, b);
+            break;
+        case "*":
+            result = m.multiplayer(a, b);
+            break;
+        case "/":
+            result = m.division(a, b);
+            break;
+        default:
+            throw new Exception("Ugyldig operation");
+    }
 
-static int add(int a, int b) { return a + b; }
+Console.WriteLine($"Resultatet er: {result}");
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Calculator_project
+
+## beskrivelse af hvordan vi vil sikre vores data under uddannelse
+
+- 
